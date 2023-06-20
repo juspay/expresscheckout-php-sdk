a6e281bab5d642ff236e521a293bd58ed525ac0c
ramprakash.v@juspay.in <ramprakash.v@juspay.in>
2023-06-20 14:50:07 +0530

    test env changes

--- tests/TestEnvironment.php
+++ tests/TestEnvironment.php
@@ -5,10 +5,16 @@ namespace Juspay\Test;
 use Juspay\JuspayEnvironment;
 
 class TestEnvironment {
-    public static $merchantId = "";
-    public static $apiKey = "";
+    public function __construct() {
+        self::$merchantId = getenv('MERCHANT_ID');
+        self::$apiKey = getenv('API_KEY');
+    }
+    public static $merchantId;
+    public static $apiKey;
     public static $baseUrl = JuspayEnvironment::SANDBOX_BASE_URL;
 }
+
+new TestEnvironment();
 JuspayEnvironment::init ()
 ->withApiKey ( TestEnvironment::$apiKey )
 ->withBaseUrl ( TestEnvironment::$baseUrl );
